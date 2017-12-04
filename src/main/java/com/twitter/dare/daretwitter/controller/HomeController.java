16763c21eb12c22d8e9f8fc09a178f45866d45fb
gaurav <gauravsonkar597@gmail.com>
2017-12-04 07:02:28 +0530

    generate parsed url

--- src/main/java/com/twitter/dare/daretwitter/controller/HomeController.java
+++ src/main/java/com/twitter/dare/daretwitter/controller/HomeController.java
@@ -2,24 +2,27 @@ package com.twitter.dare.daretwitter.controller;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twitter.dare.daretwitter.services.HomeService;
+import com.twitter.dare.twitter.models.UrlResponseVO;
 
 @RestController
 public class HomeController {
 
 	@RequestMapping(value = "/rest/home", method = RequestMethod.POST)
-	public void streamVideo() {
-		
+	public ResponseEntity<UrlResponseVO> streamVideo() {
+
 		HomeService homeService = new HomeService();
 		try {
-			homeService.streamVideo();
+			return new ResponseEntity<UrlResponseVO>(new UrlResponseVO(homeService.streamVideo()), HttpStatus.OK);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return new ResponseEntity<UrlResponseVO>(HttpStatus.NOT_FOUND);
 		}
 	}
 
--- src/main/java/com/twitter/dare/daretwitter/services/HomeService.java
+++ src/main/java/com/twitter/dare/daretwitter/services/HomeService.java
@@ -1,32 +1,58 @@
 package com.twitter.dare.daretwitter.services;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.URL;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 
 public class HomeService {
 
 	private static String bucketName = "dare-twitter";
-	private static String key = "Youtube's Smallest Video.mp4";
+	private static String objectKey = "Youtube's Smallest Video.mp4";
 
-	public void streamVideo() throws IOException {
+	@SuppressWarnings("deprecation")
+	public String streamVideo() throws IOException {
 		AmazonS3 s3Client = new AmazonS3Client(
 				new BasicAWSCredentials("", ""));
-		S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucketName, key));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(s3Object.getObjectContent()));
+		s3Client.setEndpoint("");
+		try {
+			System.out.println("Generating pre-signed URL.");
+			java.util.Date expiration = new java.util.Date();
+			long milliSeconds = expiration.getTime();
+			milliSeconds += 1000 * 10; // Add 1 hour.
+			expiration.setTime(milliSeconds);
 
-		while (true) {
-			String line;
-			line = reader.readLine();
-			if (line == null)
-				break;
-			System.out.println(line);
+			GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName,
+					objectKey);
+			generatePresignedUrlRequest.setMethod(HttpMethod.GET);
+			generatePresignedUrlRequest.setExpiration(expiration);
+
+			URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+
+			System.out.println("Pre-Signed URL = " + url.toString());
+
+			return url.toString();
+		} catch (AmazonServiceException exception) {
+			System.out.println("Caught an AmazonServiceException, " + "which means your request made it "
+					+ "to Amazon S3, but was rejected with an error response " + "for some reason.");
+			System.out.println("Error Message: " + exception.getMessage());
+			System.out.println("HTTP  Code: " + exception.getStatusCode());
+			System.out.println("AWS Error Code:" + exception.getErrorCode());
+			System.out.println("Error Type:    " + exception.getErrorType());
+			System.out.println("Request ID:    " + exception.getRequestId());
+			return null;
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, " + "which means the client encountered "
+					+ "an internal error while trying to communicate" + " with S3, "
+					+ "such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+			return null;
 		}
 	}
 
